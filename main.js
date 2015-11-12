121779f5682be7303c232218e2c4257e1d81e888
Rolando Cosme <rolandocosme@nbp-214-224.nbp.ruw.rutgers.edu>
2015-11-11 21:00:45 -0500

    added functions to ask for numbers and to compare the two input numbers

---
+++ main.js
@@ -0,0 +1,18 @@
+
+var firstNumber
+var firstNumber = prompt("Type 1st number");
+var secondNumber
+var secondNumber = prompt("Type 2nd number");
+
+console.log("firstNumber is a ", typeof firstNumber);
+console.log("secondNumber is a ", typeof secondNumber);
+
+//compare the 2 numbers
+
+if(firstNumber > secondNumber) {
+    alert("2nd number is smaller");
+  }
+
+  if(secondNumber > firstNumber) {
+    alert("1st number is smaller");
+  }
